6fefcc0542811a4fe7e8c3faf022d6aa0b78ed68
YusyMC <yusufshaheen206@gmail.com>
2025-09-13 15:31:14 +0100

    startup pygame setup

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import pygame
+
+# Initialises Pygame
+pygame.init()
+
+clock = pygame.time.Clock()
+
+# game window
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("The last Shelter TD")
+
+# game
+while True:
+    
+    clock.tick(60) # FPS
+
+    # event handler
+    for event in pygame.event.get():
+
+        # quit
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
