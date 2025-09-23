adb7577dfeac4250c5340dbaec1ff1079afb2837
YusyMC <yusufshaheen206@gmail.com>
2025-09-23 15:43:07 +0100

    Added Backrground

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from button import Button
+from PIL import Image, ImageFilter
 
 
 # Initialise's Pygame
@@ -8,10 +9,20 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-# game window
-screen = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("The last Shelter TD")
+# menu background
+imgBG = Image.open("assets/placeholderBG.jpg")
+blurredBG = imgBG.filter(ImageFilter.GaussianBlur(radius=5))
 
+mode = blurredBG.mode
+size = blurredBG.size
+data = blurredBG.tobytes()
+
+menuBG = pygame.image.fromstring(data, size, mode)
+
+# game window
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("The last Shelter TD")
+menuBG = pygame.transform.scale(menuBG, (1280,720)) # sets menu background to 1280x720 to fit screen
 
 # Button Config
 buttonSurface = pygame.image.load("assets/button.png")
@@ -35,7 +46,7 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             print("Button Registered")
     
-    screen.fill("white")
+    screen.blit(menuBG,(0,0))
     button.changeColor(pygame.mouse.get_pos())
     button.update(screen)
     pygame.display.update()
@@ -44,7 +55,7 @@ while True:
 # https://github.com/baraltech/Menu-System-PyGame/blob/main/main.py
 
 def mainMenu(): # main menu screen
-
-    return None
+    while True:
+        screen.blit(menuBG)
 
 pygame.quit()
