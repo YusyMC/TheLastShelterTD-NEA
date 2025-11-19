c8afca32b4b7767bba5c973e2b7bad0c8543a5ea
YusyMC <yusufshaheen206@gmail.com>
2025-11-19 10:26:35 +0000

    New Map implemented

--- assets.py
+++ assets.py
@@ -6,13 +6,17 @@ pygame.init()
 # MAIN MENU
 
 # Menu background 
-imgBG = Image.open("assets/placeholderBG.jpg")
-blurredBG = imgBG.filter(ImageFilter.GaussianBlur(radius=5)) # Makes it blurry
-mode = blurredBG.mode
-size = blurredBG.size
-data = blurredBG.tobytes()
+imgBG = Image.open("assets/map.png").convert("RGBA")
+
+
+#blurredBG = imgBG.filter(ImageFilter.GaussianBlur(radius=5)) # Makes it blurry
+mode = imgBG.mode
+size = imgBG.size
+data = imgBG.tobytes()
 menuBG = pygame.image.fromstring(data, size, mode)
+
 menuBG = pygame.transform.scale(menuBG, (1280,720)) # sets menu background to 1280x720 to fit screen
+
 
 # Button backboard
 
@@ -22,3 +26,10 @@ buttonBackboard = pygame.image.load("assets/menu/menu_button_backboard.png")
 
 logo = pygame.image.load("assets/menu/logo.png")
 logo = pygame.transform.scale(logo, (100,100))
+
+def loadImage(path):
+    pilImage = Image.open(path).convert("RGBA")
+    mode = pilImage.mode
+    size = pilImage.size
+    data = pilImage.tobytes()
+    return pygame.image.fromstring(data, size, mode)
